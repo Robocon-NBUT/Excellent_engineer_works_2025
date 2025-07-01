117c21da18402bfee7fa0cd857a7d294eae2814d
wei566 <1990983431@qq.com>
2025-07-01 20:05:53 +0800

    first work-蔡秀洁

---
+++ work_1/content/first work-蔡秀洁/duqu.py
@@ -0,0 +1,12 @@
+import random
+random_numbers=[str(random.randint(0,9))for _ in range(5)]
+with open(file=r"input2.txt",mode="w",encoding="utf-8")as f:
+    f.write(' '.join(random_numbers))
+print(' '.join(random_numbers))
+total=0
+for num in random_numbers:
+    total+=int(num)
+average=total/len(random_numbers)
+with open("output2.txt","w",encoding="utf-8")as f:
+    f.write(f"{average:.2f}")
+print(average)
