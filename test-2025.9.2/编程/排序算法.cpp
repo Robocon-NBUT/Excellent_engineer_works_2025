af655378b081a3ea06e8ecb01dbf5eddd62bf7af
cjh308 <1598064742@qq.com>
2025-09-02 17:24:46 +0800

    9.2作业新PR提交

---
+++ test-2025.9.2/编程/排序算法.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool cmp(int x, int y) {
+	return x < y;
+}
+int main() {
+	int n;
+	cin >> n;
+	
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	
+	sort(v.begin(), v.end(), cmp);
+	
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	return 0;
+} 
