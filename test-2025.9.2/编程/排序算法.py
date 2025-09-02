b1ee23dc53dc891a27231c2b51f0e6b4f3ca1621
cjh308 <1598064742@qq.com>
2025-09-02 21:57:56 +0800

    9.2测试

---
+++ test-2025.9.2/编程/排序算法.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+n = int(input())
+arr = list(map(int, input().split()))
+sorted_arr = bubble_sort(arr)
+print(sorted_arr)
