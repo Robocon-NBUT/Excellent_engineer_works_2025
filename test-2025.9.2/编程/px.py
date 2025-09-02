65e8e9c54128efa5b85557680345e9d54fc14df2
qzhc-9527 <2943366564@qq.com>
2025-09-02 18:58:19 +0800

    Add files via upload

---
+++ test-2025.9.2/编程/px.py
@@ -0,0 +1,13 @@
+def px(n, a):
+    a = list(map(int, a.split()))
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if a[j] < a[j+1]:
+                b = a[j]
+                a[j] = a[j+1]
+                a[j+1] = b
+    return a
+n = int(input("输入序列长度:"))
+xl = input("输入序列:")
+sxxl = px(n, xl)
+print("结果序列:", " ".join(map(str, sxxl)))
