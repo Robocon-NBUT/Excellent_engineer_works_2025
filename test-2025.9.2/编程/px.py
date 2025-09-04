bbf18973192b5bd8eef15901ad585fcfc9a1d4a8
qzhc-9527 <2943366564@qq.com>
2025-09-04 18:21:10 +0800

    Update px.py

--- test-2025.9.2/编程/px.py
+++ test-2025.9.2/编程/px.py
@@ -2,7 +2,7 @@ def px(n, a):
     a = list(map(int, a.split()))
     for i in range(n):
         for j in range(0, n-i-1):
-            if a[j] < a[j+1]:
+            if a[j] > a[j+1]:
                 b = a[j]
                 a[j] = a[j+1]
                 a[j+1] = b
@@ -10,4 +10,5 @@ def px(n, a):
 n = int(input("输入序列长度:"))
 xl = input("输入序列:")
 sxxl = px(n, xl)
+
 print("结果序列:", " ".join(map(str, sxxl)))
