452c915bc3c50323a37f42dd239a3a6356f628bb
Y666819 <3511299149@qq.com>
2025-09-04 18:50:47 +0800

    Add files via upload

---
+++ 智能建造何聖育编程test1.py
@@ -0,0 +1,7 @@
+n = int(input())
+nums = list(map(int, input().split()))
+for i in range(n):
+   for j in range(n-i-1):
+       if nums[j] > nums[j+1]:
+         nums[j], nums[j+1] = nums[j+1], nums[j]
+print(' '.join(map(str, nums)))
